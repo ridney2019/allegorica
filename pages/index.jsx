4148fdfe4d8ede53996039130046e49a97e73d10
ridney2019 <ridneygbs@gmail.com>
2021-07-13 20:24:06 -0300

    página em construção

---
+++ pages/index.jsx
@@ -0,0 +1,111 @@
+
+function home(){
+    return (
+            <>
+                <html>
+                    <head>
+                        <title>Allegorica Psicologia</title>
+                        <meta charset="utf-8"/>
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+                        <meta name="format-detection" content="telephone=no"/>
+                        <meta name="apple-mobile-web-app-capable" content="yes"/>
+
+                        <link rel="stylesheet" type="text/css" href="/assets/fonts/fontawesome/font-awesome.min.css"/>
+
+                        <link rel="stylesheet" type="text/css" href="/assets/vendors/bootstrap/grid.css"/>
+                        <link rel="stylesheet" type="text/css" href="/assets/vendors/YTPlayer/css/jquery.mb.YTPlayer.min.css"/>
+                        <link rel="stylesheet" type="text/css" href="/assets/vendors/vegas/vegas.min.css"/>
+
+                        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Work+Sans:300,400,500,700"/>
+                        <link rel="stylesheet" type="text/css" id="app-stylesheet" href="/assets/css/main.css"></link>
+                        
+                    </head>
+                    <body>
+                            <div class="page-wrap" id="root">
+
+                                <div class="md-content">
+                                    
+
+                                    <div class="hero md-skin-dark">
+                                        <div id="particles-js" data-effect="star"></div>
+                                        
+
+                                        <div class="header">
+                                            <div class="header__left">
+                                                
+                                                <span>Q. 501 Sul Av.Joaquim Teotônio Segurado, 560 
+                                                     sala 604 andar 6º - Palmas - TO</span>
+                                                <span>allegorica.psicologia@gmail.com</span>
+                                            </div>
+                                            <br />
+                                            <div class="header__social">
+                                                
+
+                                                <a class="social-icon social-icon__rounded" href="https://pt-br.facebook.com/allegorica.psicologia/"><i class="fa fa-facebook"></i></a>
+
+                                                <a class="social-icon social-icon__rounded" href="https://twitter.com/allegoricapsi?lang=bg"><i class="fa fa-twitter"></i></a>
+                                                
+                                                <a class="social-icon social-icon__rounded" href="https://www.instagram.com/allegorica.psicologia/?hl=pt-br"><i class="fa fa-instagram"></i></a>
+                                                
+                                                <a class="social-icon social-icon__rounded" href="https://www.linkedin.com/company/allegoricapsicologia/"><i class="fa fa-linkedin"></i></a>
+
+                                                <a class="social-icon social-icon__rounded" href="https://api.whatsapp.com/message/JQFENPJSPQROP1"><i class="fa fa-whatsapp"></i></a>
+                                                
+                                            </div>
+                                        </div>
+                                        
+                                        <div class="container">
+                                            <div class="hero__wrapper">
+                                                    <div class="row">
+                                                        <div class="col-lg-10 col-xs-offset-0 col-sm-offset-0 col-md-offset-0 col-lg-offset-1 ">
+                                                            <div class="hero__title_inner"><span class="hero__icon">A</span>
+                                                                <h1 class="hero__title">Site Em Construção ! </h1>
+                                                                <p class="hero__text">Em Breve Estaremos Prontos.<br/>Está ficando incrível.<br/></p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                
+        
+                                                <div class="countdown__module hide undefined" data-date="2018/5/21">
+                                                    <p><span>%D</span> Days</p>
+                                                    <p><span>%H</span> Hours</p>
+                                                    <p><span>%M</span> Minutes</p>
+                                                    <p><span>%S</span> Seconds</p>
+                                                </div>
+                                                
+                                                <div class="service-wrapper">
+                                                    
+
+                                                    <div class="service">
+                                                        <h2 class="service__title">Psicoterapia individual e em Grupo</h2>
+                                                        <p class="service__text">Atendimento presencial e Online.</p>
+                                                    </div>
+                                                    
+                                                    
+                                                    <div class="service">
+                                                        <h2 class="service__title">Orientação Profissional</h2>
+                                                        <p class="service__text">Programa de Orientação Profissional para adolescentes, jovens e adultos.</p>
+                                                    </div>
+                                                    
+
+                                                    <div class="service">
+                                                        <h2 class="service__title">Avaliação Psicológica</h2>
+                                                        <p class="service__text"> Aplicação de testes.</p>
+                                                    </div>
+                                                    
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    
+                                </div>
+
+                            </div>
+                            
+                    </body>
+                </html>
+            </>
+    )
+  }
+
+export default home
